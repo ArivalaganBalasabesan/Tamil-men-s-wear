7309b2f14f81eab2f9b4df0cfde82093d04eb8c7
Arivalagan Balasabesan <it24104300@my.sliit.lk>
2026-05-05 23:15:28 +0530

    Synchronized Products and Inventories collections using Mongoose hooks

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -13,14 +13,6 @@ exports.createProduct = async (req, res) => {
   try {
     const newProduct = new Product(req.body);
     const product = await newProduct.save();
-
-    // Create corresponding Inventory record
-    const Inventory = require('../models/Inventory');
-    await new Inventory({ 
-      product: product._id, 
-      stockLevel: product.stock || 0 
-    }).save();
-
     res.json(product);
   } catch (err) {
     res.status(500).send('Server Error');
@@ -33,13 +25,6 @@ exports.updateProduct = async (req, res) => {
     if (!product) return res.status(404).json({ msg: 'Product not found' });
     
     product = await Product.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
-
-    // Sync Inventory stock if it was updated
-    if (req.body.stock !== undefined) {
-      const Inventory = require('../models/Inventory');
-      await Inventory.findOneAndUpdate({ product: product._id }, { stockLevel: product.stock });
-    }
-
     res.json(product);
   } catch (err) {
     res.status(500).send('Server Error');
